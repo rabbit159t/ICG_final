b23417d004bf8671921cb0ab1a6ed7f4ab5fd907
rabbit159t <b00902045@gmail.com>
2016-06-13 20:47:35 +0800

    template

--- main.py
+++ main.py
@@ -4,40 +4,48 @@ import cv2,colorsys
 from matplotlib import pyplot as plt
 import math
 
-
+'''
+size = 1: i V T N-type
+size = 2: L I Y X-type
+'''
 class template():
-	def __init__(self, size, c, w):
-		self.size = size
-		self.c = c
-		self.w = w
+    def __init__(self, size, w):
+        self.size = size
+        self.w = w
+    def info(self):
+        print self.size, self.w
+t = []
+t.append(template(1, [5]))
+t.append(template(1, [45]))
+t.append(template(1, [90]))
 
+'''
+r: clockwise
+l: counter clockwise
 
-x = template(2,1,10)
-print x.size, x.c, x.w
-# r: clockwise
-# l: counter clockwise
-#
-# return
-#   (+) : clockwise
-#   (-) : counter clockwise
+return
+    (+) : clockwise
+    (-) : counter clockwise
+'''
 def func(c,h):
     if c > h:
         r = c-h
-        #l = 360-r
     else:
         r = c+360-h
     l = 360-r
     if(r<=l): return r
     else: return -l
 
-
-w = 10
+#========TODO: choose template=====================
+w = t[1].w[0]
 size = 31
 window = signal.gaussian(size, std=0.5*w)
 interval = 360/(size-1)
 print window, interval
-center = 0
+
 img = cv2.imread('01.jpg')
+height, width = img.shape[:2]
+img  = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 #bgdModel = np.zeros((1,65),np.float64)
 #fgdModel = np.zeros((1,65),np.float64)
 
@@ -47,12 +55,17 @@ img = cv2.imread('01.jpg')
 #mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
    
 #img = img*mask2[:,:,np.newaxis]
-height, width = img.shape[:2]
-img  = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+total=0
+for i in range(height):
+    for j in range(width):
+        total+=img[i][j][0]
+center = total/height/width
+print center
+
 print img[0][0][0]
 for i in range(height):
     for j in range(width):
-        hue = img[i][j][0]*2
+        hue = img[i][j][0]<<1
         
         #print img[i][j][0],func(center,img[i][j][0]),img[i][j][0],window[func(center,img[i][j][0])]
         #print img[i][j][0],func(center,img[i][j][0]),int(0.5*w*(window[func(center,img[i][j][0])/6])+center),
@@ -63,7 +76,7 @@ for i in range(height):
             hue = hue -360
         elif hue < 0 :
             hue = hue + 360
-        img[i][j][0] = hue/2
+        img[i][j][0] = hue>>1
 img  = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
 
 plt.imshow(img),plt.colorbar(),plt.show()
