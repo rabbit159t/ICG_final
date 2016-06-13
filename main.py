74f2bc37f8f55c5de6ac2d05006dba7cf6939d88
rabbit159t <b00902045@gmail.com>
2016-06-13 22:01:20 +0800

    test other img QQ

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ size = 1: i V T N-type
 size = 2: L I Y X-type
 '''
 class template():
-    def __init__(self, size, w):
-        self.size = size
+    def __init__(self, w):
         self.w = w
+    
     def info(self):
-        print self.size, self.w
+        print self.w
 t = []
-t.append(template(1, [5]))
-t.append(template(1, [45]))
-t.append(template(1, [90]))
+t.append(template([5]))
+t.append(template([45]))
+t.append(template([90]))
+t.append(template([0.0001]))
 
 '''
 r: clockwise
@@ -37,13 +38,13 @@ def func(c,h):
     else: return -l
 
 #========TODO: choose template=====================
-w = t[1].w[0]
+w = t[2].w[0]
 size = 31
-window = signal.gaussian(size, std=0.5*w)
+window = signal.gaussian(size, std=w>>1)
 interval = 360/(size-1)
 print window, interval
 
-img = cv2.imread('01.jpg')
+img = cv2.imread('03.jpg')
 height, width = img.shape[:2]
 img  = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 #bgdModel = np.zeros((1,65),np.float64)
@@ -60,24 +61,29 @@ for i in range(height):
     for j in range(width):
         total+=img[i][j][0]
 center = total/height/width
-print center
+if center >= 360: center = center -360
 
-print img[0][0][0]
+
+#print img[0][0][0]
 for i in range(height):
     for j in range(width):
         hue = img[i][j][0]<<1
+        turn = func(center, hue)
         
         #print img[i][j][0],func(center,img[i][j][0]),img[i][j][0],window[func(center,img[i][j][0])]
         #print img[i][j][0],func(center,img[i][j][0]),int(0.5*w*(window[func(center,img[i][j][0])/6])+center),
-        
-        hue = int(center+0.5*w*(window[ int((func(center,hue)+180)/interval) ]))
+        if abs(turn) < w: continue
+        if turn >= 0: hue = int(center + 0.5*w*(window[ int((turn+180)/interval) ]))
+        else: hue = int(center - 0.5*w*(window[ int((turn+180)/interval) ]))
         #print hue,center
         if hue >= 360:
             hue = hue -360
         elif hue < 0 :
             hue = hue + 360
         img[i][j][0] = hue>>1
-img  = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
 
+print 'test: ', hue,center
+print 'center in colorbar: ', center*4/3
+img  = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
 plt.imshow(img),plt.colorbar(),plt.show()
 
